d073fa310eef27f59a157013ada680273cb33b62
mukhin-sn <senikomu@gmail.com>
2024-02-15 01:56:19 +0700

    creation of the required functionality

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import CategoryListView, contacts, ProductDetailView, ProductCategoryListView
+from catalog.views import CategoryListView, contacts, ProductDetailView, ProductCategoryListView, ProductCreateView
 from catalog.views import BlogListView, BlogDetailView, BlogCreateView, BlogUpdateView, BlogDeleteView
 
 app_name = CatalogConfig.name
@@ -8,6 +8,7 @@ app_name = CatalogConfig.name
 urlpatterns = [
     path('', CategoryListView.as_view(), name='index'),
     path('base/', contacts, name='contacts'),
+    path('product_create/', ProductCreateView.as_view(), name='product_create'),
     path('product/<int:pk>/', ProductDetailView.as_view(), name='product'),
     path('category/<int:pk>/', ProductCategoryListView.as_view(), name='category'),
     path('create/', BlogCreateView.as_view(), name='create_blog'),
