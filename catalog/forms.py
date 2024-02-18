f19f05ce75e0d6e2de99ae828b0629004f56faae
mukhin-sn <senikomu@gmail.com>
2024-02-19 03:02:43 +0700

    creation of clean-methods

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 
 from catalog.models import Product
+from config.settings import PROHIBITED_COMBINATIONS
 
 
 class ProductForm(forms.ModelForm):
@@ -9,3 +10,48 @@ class ProductForm(forms.ModelForm):
         fields = '__all__'
         # fields = ('name', 'category',)
         # exclude = ('image',)
+
+    def word_check(self, var: str, *args, **kwargs) -> bool:
+        """
+        Метод формирования списка слов из вводимого предложения
+        и его валидации.
+        Слитно написанные несколько запрещенных слов (без разделяющих символов)
+        валидацию пройдут.
+        Возвращает True, если валидация прошла успешно.
+        """
+        alphabet = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+        data_str = self.cleaned_data.get(var)
+        out_list = []
+
+        # сделаем список из строки
+        data_list = data_str.lower().split(' ')
+
+        # уберем все лишние символы из слов
+        for word in data_list:
+            var_word = ''
+            for letter in word:
+                if letter in alphabet:
+                    var_word += letter
+                else:
+                    out_list.append(var_word)
+                    var_word = ''
+            out_list.append(var_word)
+
+        for word in out_list:
+            if word in args:
+                raise forms.ValidationError('Присутствуют запрещенные слова')
+        return True
+
+    def clean_name(self):
+        """
+        clean-метод для поля name продукта
+        """
+        if self.word_check('name', *PROHIBITED_COMBINATIONS):
+            return self.cleaned_data.get('name')
+
+    def clean_description(self):
+        """
+        clean-метод для поля description продукта
+        """
+        if self.word_check('description', *PROHIBITED_COMBINATIONS):
+            return self.cleaned_data.get('description')
